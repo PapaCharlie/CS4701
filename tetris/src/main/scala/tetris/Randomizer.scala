4bed9378ddd9e40b4c5bf0e7b9222aa539708d49
Paul Chesnais <paul.chesnais@gmail.com>
2015-10-19 13:23:45 -0400

    Now we have a randomizer

--- tetris/src/main/scala/tetris/Randomizer.scala
+++ tetris/src/main/scala/tetris/Randomizer.scala
@@ -2,6 +2,9 @@ package tetris
 
 import tetris.tetrominoes._
 import java.util.Random
+
+import scala.collection.mutable.Queue
+
 //import scala.collection.mutable.Array
 
 /**
@@ -11,18 +14,39 @@ class Randomizer {
 
   private val rng = new Random()
 
-  private val pieces: Seq[Tetromino] = Array(new J, new L, new O, new S, new T, new Z)
+  private val pieces: Seq[Tetromino] = Array(new J, new L, new O, new S, new T, new Z, new I)
 
   var bag: Array[Tetromino] = pieces.flatMap(piece => (1 to 5).map(_ => piece.copy)).toArray
 
   var history: Seq[Tetromino] = Seq()
 
-  def next: Tetromino = {
+  private val toCome: Queue[Tetromino] = Queue()
+
+  private def pickAndEnqueue = {
     val n = rng.nextInt(35)
     val letter = bag(n)
     history :+= letter
     bag(n) = history.head
-    letter
+    toCome.enqueue(letter)
+  }
+
+  def next: Tetromino = {
+    pickAndEnqueue
+    toCome.dequeue
+  }
+
+  /**
+   * Non-destructive
+   */
+  def preview(n: Int = 1): Seq[Tetromino] = {
+    if (toCome.length >= n) {
+      toCome.slice(0, n).toSeq
+    } else {
+      while (toCome.length != n) {
+        pickAndEnqueue
+      }
+      toCome.slice(0, n).toSeq
+    }
   }
 
 }
---
+++ tetris/src/main/scala/tetris/tetrominoes/I.scala
@@ -0,0 +1,22 @@
+package tetris.tetrominoes
+
+/**
+ * Created by papacharlie on 10/19/15.
+ */
+case class I(x: Int = 0, rotation: Int = 0) extends Tetromino {
+
+  val shape0 = Seq((0, -1), (0, 1), (0, 2))
+  val shape1 = Seq((-1, 0), (1, 0), (2, 0))
+
+  var currentShape = rotation match {
+    case 0 => shape1
+    case 1 => shape0
+  }
+
+  def rotate = S(x, (rotation + 1) % 2)
+
+  def allRotations: Seq[S] = (0 to 1).map(new S(x, _))
+
+  def copy(x: Int = 0, rotation: Int = 0): S = new S(x, rotation)
+
+}
