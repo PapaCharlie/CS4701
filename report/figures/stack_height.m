0b43663d4f31171ce1f225beb9c494fc5eaed3dc
Paul Chesnais <paul.chesnais@gmail.com>
2015-12-17 20:05:05 -0500

    Graphs

---
+++ report/figures/stack_height.m
@@ -0,0 +1,14 @@
+heights = 5:15;
+means_d4 = [509.6,378.9,765.2,481.7,870.2,903.0,570.2,543.5,407.6,376.0,369.9];
+fig = figure;
+plot(heights, means_d4, 'b+-', 'LineWidth', 2);
+xlabel 'Maximum Stack Height'
+ylabel 'Consecutive Tetrominoes Placed'
+saveas(fig, 'height_v_time_d4.pdf');
+
+% means_d5 = [509.6,378.9,765.2,481.7,870.2,903.0,570.2,543.5,407.6,376.0,369.9];
+% fig = figure;
+% plot(heights, means_d5, 'b+-', 'LineWidth', 2);
+% xlabel 'Maximum Stack Height'
+% ylabel 'Consecutive Tetrominoes Placed'
+% saveas(fig, 'figures/height_v_time_d5.pdf');
