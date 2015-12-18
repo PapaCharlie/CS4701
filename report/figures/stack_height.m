97b2b15aaa11d79305d10de21841c2b0749ebfc6
Paul Chesnais <paul.chesnais@gmail.com>
2015-12-17 20:48:21 -0500

    Out of battery, will write out results and conclusion at home

--- report/figures/stack_height.m
+++ report/figures/stack_height.m
@@ -1,10 +1,11 @@
 heights = 5:15;
 means_d4 = [509.6,378.9,765.2,481.7,870.2,903.0,570.2,543.5,407.6,376.0,369.9];
 fig = figure;
-plot(heights, means_d4, 'b+-', 'LineWidth', 2);
+plot(heights, means_d4, 'r+-', 'LineWidth', 2);
 xlabel 'Maximum Stack Height'
 ylabel 'Consecutive Tetrominoes Placed'
 saveas(fig, 'height_v_time_d4.pdf');
+close all
 
 % means_d5 = [509.6,378.9,765.2,481.7,870.2,903.0,570.2,543.5,407.6,376.0,369.9];
 % fig = figure;
